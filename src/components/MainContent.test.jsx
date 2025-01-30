a1e5a88568c5985866e36ab96d6b011979c4e085
missShevel <o.v.shevel@gmail.com>
2025-01-30 14:23:05 +0200

    test dofferent workflows

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area22')).toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
